5b5f4a10f22c556652a6b55ab473b2b507bbf8f1
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-04-29 16:58:43 +0200

    input-task

--- tasc-app/src/App.vue
+++ tasc-app/src/App.vue
@@ -1,41 +1,54 @@
 <script lang="ts" setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
+import TaskForm from './components/TaskForm.vue';
+
 const message = ref("Tasks App");
-const newTask = ref("");
-const addTask = ref("")
 </script>
 
 <template>
   <main>
-  <h1>{{message}}</h1>
-  <form>
-    <div class="form-control">
-          <label for="name">New Task</label>
-          <input v-model ="newTask" name="newTask"  placeholder="Enter your task" />   
-          <button @click="addTask">add</button>
-    </div>
-  </form>
-  <p class="">{{ newTask }}</p>
-</main>
+    <h1>{{ message }}</h1>
+    <TaskForm />
+  </main>
 </template>
 
+
 <style scoped>
-main{
+main {
   max-width: 800px;
   margin: 1rem auto;
   font-family: cursive;
 }
 
 label {
-    line-height: 1.9rem;
-  }
-  input,
-    textarea {
-    width: 100%;
-    padding: 5px;
-    outline: none;
-  }
-  fieldset {
-   padding: 20px 40px;
- }
+  line-height: 1.9rem;
+}
+
+input,
+textarea {
+  width: 100%;
+  padding: 5px;
+  outline: none;
+  margin: 0;
+}
+
+fieldset {
+  padding: 20px 40px;
+}
+
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.btn {
+  margin-top: 20px;
+  width: 200px;
+  height: auto;
+}
+
+.ok {
+  margin-top: 20px;
+  background-color: red;
+}
 </style>
---
+++ tasc-app/src/components/TaskForm.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const newTask = ref("");
+
+function formSubmitted(){
+console.log(newTask)
+}
+</script>
+<template>
+    <form @submit.prevent="formSubmitted">
+            <label for="name">New Task</label>
+            <input v-model="newTask" name="newTask" placeholder="Enter your task" />
+            <div class="button-container">
+                <button>Add</button>
+            </div>
+        
+    </form>
+</template>
